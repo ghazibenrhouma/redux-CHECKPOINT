8ccdc2593dfbb02cf5e451e27ce897c0b51c43d2
ghazibenrhouma <ghazybenrhouma@gmail.com>
2020-09-30 14:38:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector,useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
+
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center", border:"1px solid #ddd", height:"300px", backgroundColor:"#dddd",fontFamily:"italic"}}>
+        <h1>Counter {counter}</h1>
+        <button style={{width:"50px", backgroundColor:"#3ee8c8",borderRadius:'5px' ,border:'0px ',marginRight:'10px'}} onClick={() => dispatch(increment())}>+</button>
+       
+        <button style={{width:"50px", backgroundColor:"#e83c3c",borderRadius:'5px' ,border:'0px '}}onClick={() => dispatch(decrement())}>-</button>
+        {isLogged ? <h3>Valuable information i shouldn't see</h3> : '' }
     </div>
   );
 }
